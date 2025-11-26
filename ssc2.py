4f4561b072d5dcab73cbed31e026c1087bce04ce
Saravana Kumar <saravanakumar@Saravanas-MacBook-Air.local>
2025-11-26 19:54:40 +0530

    update - 2025-11-24_07:31

--- ssc2.py
+++ ssc2.py
@@ -3,14 +3,38 @@ import numpy as np
 import ut_tools_ssc as ssc
 import streamlit as st
 from app import eod
+from barchart import plot_tv_ohlc_dark_v2
 
 
 def main():
-    df = eod("NIFTY", "2023-01-01", "2023-12-31")
+    st.set_page_config(page_title="SSC Data Viewer", layout="wide")
+    df = eod("CRUDEOIL", "2023-01-01", "2023-12-31")
+    df["date"] = pd.to_datetime(df["date"])
+    df.set_index("date", inplace=True)
     df = ssc.SSC(df)
+    df = df[:-100]
+    df["x"] = range(len(df))
+    tops = [
+        (row["x"], row["high"])
+        for _, row in df[df["swing_point"] == df["high"]].iterrows()
+    ]
 
+    bottoms = [
+        (row["x"], row["low"])
+        for _, row in df[df["swing_point"] == df["low"]].iterrows()
+    ]
+
+    print(tops, bottoms)
+
+    fig = plot_tv_ohlc_dark_v2(
+        df,
+        title="SSC Chart",
+        tops=tops,
+        bottoms=bottoms,
+    )
+    st.pyplot(fig)
     st.title("SSC Data Viewer")
-    st.dataframe(df[["date", "open", "high", "low", "close", "bar_type"]])
+    st.dataframe(df[["open", "high", "low", "close", "bar_type", "swing_point"]])
 
 
 if __name__ == "__main__":
--- ut_tools_ssc.py
+++ ut_tools_ssc.py
@@ -1,33 +1,24 @@
 import pandas as pd
+import numpy as np
 
 
 def barType(previous_bar, current_bar) -> str:
-    if (
-        current_bar["high"] > previous_bar["high"]
-        and current_bar["low"] >= previous_bar["low"]
-    ):
-        return "DB"
+    previous_h, previous_l = previous_bar["high"], previous_bar["low"]
+    current_h, current_l = current_bar["high"], current_bar["low"]
 
-    if (
-        current_bar["high"] <= previous_bar["high"]
-        and current_bar["low"] < previous_bar["low"]
-    ):
-        return "DB"
-
-    if (
-        current_bar["high"] < previous_bar["high"]
-        and current_bar["low"] > previous_bar["low"]
-    ):
+    if current_h < previous_h and current_l > previous_l:
         return "ISB"
 
-    if (
-        current_bar["high"] > previous_bar["high"]
-        and current_bar["low"] < previous_bar["low"]
-    ):
+    if current_h > previous_h and current_l < previous_l:
         return "OSB"
+
+    return "DB"
 
 
 def barDefinition(df: pd.DataFrame) -> pd.DataFrame:
+    # Bar type assignment
+    # DB - Directional Bar, ISB - Inside Bar, OSB - Outside Bar
+
     df = df.copy()
     df["bar_type"] = ""
     df.at[df.index[0], "bar_type"] = "DB"
@@ -36,18 +27,16 @@ def barDefinition(df: pd.DataFrame) -> pd.DataFrame:
         current_bar = df.iloc[i]
         df.at[df.index[i], "bar_type"] = barType(previous_bar, current_bar)
 
-    df = multipleISBs(df)
-    return df
+    # Refine DB and OSB bars based on the last DB bar
+    # Handleling multiple ISB's in a row
 
-
-def multipleISBs(df: pd.DataFrame) -> pd.DataFrame:
-
-    df = df.copy()
     previous_db_index = 0
     for i in range(1, len(df)):
         previous_db_bar = df.iloc[previous_db_index]
 
-        if df.iloc[i]["bar_type"] == "DB" and df.iloc[i - 1]["bar_type"] == "ISB":
+        if (
+            df.iloc[i]["bar_type"] == "DB" or df.iloc[i]["bar_type"] == "OSB"
+        ) and df.iloc[i - 1]["bar_type"] == "ISB":
             df.at[df.index[i], "bar_type"] = barType(previous_db_bar, df.iloc[i])
         elif df.iloc[i]["bar_type"] == "DB" or df.iloc[i]["bar_type"] == "OSB":
             previous_db_index = i
@@ -55,10 +44,64 @@ def multipleISBs(df: pd.DataFrame) -> pd.DataFrame:
     return df
 
 
+def SwingPoints(df: pd.DataFrame) -> pd.DataFrame:
+    df = df.copy()
+    df["swing_point"] = np.nan
+    current_swing = ""
+    current_swing_index = 0
+    previous_db_index = 0
+
+    for i in range(1, len(df) - 1):
+        previous_h, previous_l = df.iloc[i - 1]["high"], df.iloc[i - 1]["low"]
+        current_h, current_l, current_bar = (
+            df.iloc[i]["high"],
+            df.iloc[i]["low"],
+            df.iloc[i]["bar_type"],
+        )
+
+        if current_bar == "ISB":
+            continue
+
+        if current_bar == "OSB":
+            if current_swing == "low":
+                if current_l < df.iloc[current_swing_index]["low"]:
+                    df.at[df.index[current_swing_index], "swing_point"] = np.nan
+                    current_swing_index = i
+                    df.at[df.index[current_swing_index], "swing_point"] = current_l
+            if current_swing == "high":
+                if current_h > df.iloc[current_swing_index]["high"]:
+                    df.at[df.index[current_swing_index], "swing_point"] = np.nan
+                    current_swing_index = i
+                    df.at[df.index[current_swing_index], "swing_point"] = current_h
+
+            continue
+
+        if current_bar == "DB":
+            previous_db_index = i
+            if current_h >= previous_h:
+                if current_swing == "low":
+                    continue
+
+                df.at[df.index[previous_db_index - 1], "swing_point"] = previous_l
+                current_swing = "low"
+                current_swing_index = i - 1
+                previous_db_index = i - 1
+            if current_l <= previous_l:
+                if current_swing == "high":
+                    continue
+                df.at[df.index[previous_db_index - 1], "swing_point"] = previous_h
+                current_swing = "high"
+                current_swing_index = i - 1
+                previous_db_index = i - 1
+                pass
+        pass
+
+    return df
+
+
 def SSC(df: pd.DataFrame) -> pd.DataFrame:
     df = df.copy()
-    df["SSC"] = 0.0
-
     df = barDefinition(df)
+    df = SwingPoints(df)
 
     return df
